0cde99a2f93fc9ce34e6b3a8dc06e21b597bc087
Anas Mohamed <amohamed2479@gmail.com>
2021-09-05 18:45:00 -0500

    move the `html` and `<style>` to the bottom of the page

--- src/components/tutorial.svelte
+++ src/components/tutorial.svelte
@@ -1,32 +1,8 @@
-<div class="flex flex-col md:flex-row flex-wrap min-h-screen">
-    <div class=" flex items-center flex-1 min-h-full bg-gray-50">
-        <div class="container p-10">  
-                <div class="content">
-                    <h1 class="text-2xl text-gray-700">
-                        {title}
-                    </h1>
-                    <p class="text-base text-gray-600">
-                        {description}
-                    </p>
-                </div>
-        </div>
-    </div>
-    <div class="flex flex-1 items-center p-10">
-        <div bind:this="{el}" class="bash-mirror transform md:-translate-x-20 shadow-2xl -translate-y-20 md:translate-y-0 ">
-            <div class="bash-mirror-header flex items-center">
-                <div class="bg-gray-300 w-4 h-4 ml-2 rounded-full"></div>
-                <div class="bg-gray-300 w-4 h-4 ml-2 rounded-full"></div>
-                <div class="bg-gray-300 w-4 h-4 ml-2 rounded-full"></div>
-            </div>
-    
-        </div>
-    </div>
-    </div>
-    
-    <script>
-    import { onMount } from "svelte";
-    import {fade,fly} from 'svelte/transition'
-    import {Bash} from '@anas2479/bash-mirror'
+<script>
+  import { onMount } from "svelte";
+  import { fade, fly } from "svelte/transition";
+  import _ from "lodash";
+  import { Bash } from "@anas2479/bash-mirror";
 
 
     let el
@@ -76,4 +52,35 @@
             width: 100%;
         }
 
-    </style>
+    </style>  <div class=" flex items-center flex-1 min-h-full bg-gray-50">
+      <div class="content p-10 max-w-[600px]">
+        <h1 bind:this="{title}" class="text-2xl text-gray-700">
+          
+        </h1>
+        <p bind:this="{description}" class="text-base text-gray-600">
+          
+        </p>
+      </div>
+  </div>
+  <div class="flex flex-1 items-center p-10">
+    <div
+      bind:this={el}
+      class="bash-mirror transform md:-translate-x-20 shadow-2xl -translate-y-20 md:translate-y-0 "
+    >
+      <div class="bash-mirror-header flex items-center">
+        <div class="bg-gray-300 w-4 h-4 ml-2 rounded-full" />
+        <div class="bg-gray-300 w-4 h-4 ml-2 rounded-full" />
+        <div class="bg-gray-300 w-4 h-4 ml-2 rounded-full" />
+        <span class="text-xs mx-auto text-gray-400">Tutorial CLI</span>
+      </div>
+    </div>
+  </div>
+</div>
+
+<style>
+  @import url("https://cdn.jsdelivr.net/npm/@anas2479/bash-mirror@2.2.4/lib/css/bash-mirror.css");
+
+  .bash-mirror {
+    width: 100%;
+  }
+</style>
