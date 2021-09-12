b6ba9c5c0f3a829afb6c0871c874e2382fa0f399
Anas Mohamed <amohamed2479@gmail.com>
2021-09-12 11:47:01 -0500

    Create `LessonLink`
    Create a component that takes the user to a specific lesson in the tutorial.

---
+++ src/components/lesson-link.svelte
@@ -0,0 +1,28 @@
+<script>
+  import { createEventDispatcher, onMount } from "svelte";
+
+  export let title;
+  export let description;
+  export let lesson;
+
+  let dispatch = createEventDispatcher();
+
+  function returnLesson() {
+    dispatch("lesson", {
+      name: lesson,
+    });
+  }
+</script>
+
+<div
+  on:click={returnLesson}
+  class=" overflow-hidden cursor-pointer shadow-2xl rounded-lg ring-1 ring-black ring-opacity-5 bg-white max-w-[300px]"
+>
+  <div class="px-4 py-3">
+    <h3 class="text-lg md:text-lg leading-8 font-medium text-gray-700">
+      {title}
+    </h3>
+    <p class="text-xs md:text-base eading-5 font-normal text-gray-600">{description}</p>
+  </div>
+  <slot />
+</div>
