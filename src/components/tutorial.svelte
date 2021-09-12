6c5ce7667b5f8fac5c464237cbb5fb797b15fe8b
Anas Mohamed <amohamed2479@gmail.com>
2021-09-12 12:09:35 -0500

    clean up component HTML

--- src/components/tutorial.svelte
+++ src/components/tutorial.svelte
@@ -113,26 +113,23 @@
 </div>
 <div class="flex flex-col md:flex-row flex-wrap min-h-screen">
   <div class=" flex items-center flex-1 min-h-full bg-gray-50">
-      <div class="content p-10 max-w-[600px]">
-        <h1 bind:this="{title}" class="text-2xl text-gray-700">
-          
-        </h1>
-        <p bind:this="{description}" class="text-base text-gray-600">
-          
-        </p>
-      </div>
+    <div class="content p-10 max-w-[600px] mt-24 mb-5">
+      <div class="mb-3 flex text-gray-500">Try out what you learn using the shell <div class=" max-w-[30px] mx-1 transform md:-rotate-90">👇</div> </div>
+      <h1 bind:this={title} class="text-2xl text-gray-700" />
+      <p bind:this={description} class="text-base text-gray-600" />
+    </div>
   </div>
   <div class="flex flex-1 items-center p-10">
     <div
       bind:this={el}
       class="bash-mirror transform md:-translate-x-20 shadow-2xl -translate-y-20 md:translate-y-0 "
     >
-      <div class="bash-mirror-header flex items-center">
-        <div class="bg-gray-300 w-4 h-4 ml-2 rounded-full" />
-        <div class="bg-gray-300 w-4 h-4 ml-2 rounded-full" />
-        <div class="bg-gray-300 w-4 h-4 ml-2 rounded-full" />
-        <span class="text-xs mx-auto text-gray-400">Tutorial CLI</span>
-      </div>
+    <div class="bash-mirror-header flex items-center">
+      <div class="bg-gray-300 w-4 h-4 ml-2 rounded-full" />
+      <div class="bg-gray-300 w-4 h-4 ml-2 rounded-full" />
+      <div class="bg-gray-300 w-4 h-4 ml-2 rounded-full" />
+      <span class="text-xs mx-auto text-gray-400">Tutorial CLI</span>
+    </div>
     </div>
   </div>
 </div>
@@ -144,3 +141,4 @@
     width: 100%;
   }
 </style>
+
