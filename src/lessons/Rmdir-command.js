e970dab56427de0fdd1492c3997e59c7e4276035
Anas Mohamed <amohamed2479@gmail.com>
2021-09-12 17:00:23 -0500

    create the rmdir lesson

---
+++ src/lessons/Rmdir-command.js
@@ -0,0 +1,24 @@
+
+const checkAnswer = require('../utilities/checkAnswer')
+const _ =  require('lodash')
+const Lesson = require('./Lesson')
+
+module.exports = class Rmdir_command extends Lesson{
+    constructor(title, description, shell){
+        super()
+
+        title.innerHTML = `The RMDIR command stands for remove directory. It's used to remove a folder from the current folder. You'd type "rmdir" followed by the folder name you want to remove.`
+
+        description.innerHTML = ``
+
+        shell.input(`Try <span class="text-gray-300">rmdir example</span>`,(answer)=>{
+            let userResponce = _.words(answer)
+            checkAnswer(userResponce[0], 'rmdir', ()=>{
+                shell.runCommand('rmdir', _.slice(userResponce,1,1000))
+
+                title.innerHTML = `Greate! The folder "<span class="text-gray-600">${_.slice(userResponce,1,1000)}</span>" was removed.`
+                
+            })
+        })
+    }
+}
