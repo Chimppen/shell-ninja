670525b28af84d5af55a94f8b7162e2511e8f1f9
Anas Mohamed <amohamed2479@gmail.com>
2021-09-12 12:10:34 -0500

    Create the LS command lesson

---
+++ src/lessons/ls-command.js
@@ -0,0 +1,30 @@
+const checkAnswer = require("../utilities/checkAnswer")
+const Cd_command = require("./cdCommand")
+const Lesson = require('./Lesson')
+
+
+module.exports = class Ls_command extends Lesson{
+    constructor(title, description, shell){
+        super()
+        
+        title.innerHTML = `The <span class="font-bold">LS</span> command simply lists all the items in the current folder. `
+
+        description.innerHTML = ``
+
+        shell.input(`Go ahead, run the LS command below.`, (answer)=>{
+            checkAnswer(answer,'ls', ()=>{
+                shell.runCommand('ls')
+
+                title.innerHTML =`Awesome! Notice how a list of an example folder and a file were returned.`
+                description.innerHTML = `The ls command returns the contents of the current folder.`
+
+                shell.input(`Next lesson? y/n`,(nextLesson)=>{
+                    checkAnswer(nextLesson, 'y', ()=>{
+                        shell.clear()
+                        new Cd_command(title, description, shell)
+                    })
+                })
+            })
+        })
+    }
+}
